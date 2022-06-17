00695fc016c7783d978fb99996d66bf385eba008
Damon Chen <542064784@qq.com>
2022-06-17 18:22:30 +0800

    添加web demo

---
+++ web/README.md
@@ -0,0 +1,13 @@
+# 工程简介
+spring boot 的demo工程，引入
+
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-web</artifactId>
+    </dependency>
+    
+就是一个web项目
+
+
+# 延伸阅读
+
---
+++ web/src/main/java/com/springboot/web/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.springboot.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Damon Chen
+ * @date 2022/06/17
+ */
+@RestController
+@Slf4j
+@RequestMapping("/web")
+public class WebController {
+
+    @GetMapping("/hello")
+    public String web(final String name) {
+        return "hello " + name;
+    }
+
+
+}
